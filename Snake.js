c04261888ea55219eea4ef7c50deefdfcfde8989
lucasvuong <59907180+lucasvuong@users.noreply.github.com>
2020-04-19 12:20:23 -0700

    adding controls

--- Snake.js
+++ Snake.js
@@ -22,13 +22,28 @@ function moveSnake() {
         Snake[i] = Snake[i - 1];
     }
     let head = Snake[0];
-    if (direction == "up") {
-        head[1] += 10;
-    } else if (direction == "left") {
-        head[0] -= 10;
-    } else if (direction == "down") {
-        head[1] -= 10;
-    } else {
-        head[0] += 10;
+    switch (direction) {
+        case "up":
+            head[1] += 10;
+        case "left":
+            head[0] -= 10;
+        case "down":
+            head[1] -= 10;
+        case "right":
+            head[0] += 10;
+    }
+}
+
+function keyPressed() {
+    if (oppositeDirection != key) {
+        if (key == "ArrowUp") {
+            direction = "up";
+        } else if (key == "ArrowLeft") {
+            direction = "left";
+        } else if (key == "ArrowDown") {
+            direction = "down";
+        } else if (key == "ArrowRight") {
+            direction = "right";
+        }
     }
 }
